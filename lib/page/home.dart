c710a81bdd5c561543c46164ffc2cbe11857340a
504697121@qq.com <lpy@lvtudiandian.com>
2019-04-15 11:07:58 +0800

    增加tab navbar

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -20,9 +20,56 @@ class HomeWidget extends StatefulWidget {
   }
 }
 
-class _HomeState extends State<HomeWidget> {
+class _HomeState extends State<HomeWidget> with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  List tabs = ["a", "b", "c"];
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: tabs.length, vsync: this);
+    _tabController.addListener(() {
+      print(_tabController.index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("HOME");
+    return Scaffold(
+      appBar: AppBar(
+        bottom: TabBar(
+            //生成Tab菜单
+            controller: _tabController,
+            tabs: tabs.map((e) => Tab(text: e)).toList()),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: tabs.map((e) {
+          //创建3个Tab页
+          return Container(
+            alignment: Alignment.center,
+            child: Text(e, textScaleFactor: 5),
+          );
+        }).toList(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("首页")),
+          BottomNavigationBarItem(icon: Icon(Icons.reorder), title: Text('订单')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.perm_identity), title: Text('我')),
+        ],
+        currentIndex: _selectedIndex,
+        fixedColor: Colors.red,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
