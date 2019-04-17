2ccc2685872f5557229e11192d85c358e96d5656
504697121@qq.com <lpy@lvtudiandian.com>
2019-04-17 11:11:08 +0800

    底部消息红点

--- lib/page/mainScreen.dart
+++ lib/page/mainScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'home.dart';
 import 'order.dart';
 import 'mine.dart';
+import 'package:model_mall/view/bottom_bar/bottom_tab_bar.dart';
 
 class MainScreenPage extends StatefulWidget {
   @override
@@ -13,22 +14,37 @@ class MainScreenPage extends StatefulWidget {
 class _MainScreenState extends State<MainScreenPage>
     with SingleTickerProviderStateMixin {
   int _selectedIndex = 0;
+  String badgeNo1;
   List _bodys = [HomePage(), OrderPage(), MinePage()];
+  var _titles = ['首页', '订单', '我'];
+  var _icons = [Icons.home, Icons.reorder, Icons.perm_identity];
+
+  @override
+  void initState() {
+    badgeNo1 = '11';
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _bodys[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("首页")),
-          BottomNavigationBarItem(icon: Icon(Icons.reorder), title: Text('订单')),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.perm_identity), title: Text('我')),
+      bottomNavigationBar: BottomTabBar(
+        items: <BottomTabBarItem>[
+          BottomTabBarItem(
+              icon: Icon(_icons[0]),
+              title: Text(_titles[0]),
+              badgeNo: badgeNo1),
+          BottomTabBarItem(icon: Icon(_icons[1]), title: Text(_titles[1])),
+          BottomTabBarItem(icon: Icon(_icons[2]), title: Text(_titles[2])),
         ],
-        currentIndex: _selectedIndex,
         fixedColor: Colors.red,
+        currentIndex: _selectedIndex,
         onTap: _onItemTapped,
+        type: BottomTabBarType.fixed,
+        isAnimation: false,
+        isInkResponse: false,
+        badgeColor: Colors.red,
       ),
     );
   }
